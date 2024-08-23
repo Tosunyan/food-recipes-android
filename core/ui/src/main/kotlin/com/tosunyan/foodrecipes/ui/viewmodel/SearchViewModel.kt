c78ed40ae3d9ac69fe9a5e49cafafca673cdd26a
Sergey Tosunyan <sergeytosunyan33@gmail.com>
2024-08-23 11:11:13 +0400

    Implement debounce for search input

--- core/ui/src/main/kotlin/com/tosunyan/foodrecipes/ui/viewmodel/SearchViewModel.kt
+++ core/ui/src/main/kotlin/com/tosunyan/foodrecipes/ui/viewmodel/SearchViewModel.kt
@@ -6,13 +6,18 @@ import com.tosunyan.foodrecipes.common.utils.replace
 import com.tosunyan.foodrecipes.data.repositories.MealRepository
 import com.tosunyan.foodrecipes.data.repositories.SearchRepository
 import com.tosunyan.foodrecipes.model.MealDetailsModel
-import com.tosunyan.foodrecipes.network.api.ApiResponse
+import com.tosunyan.foodrecipes.network.api.onSuccess
 import com.tosunyan.foodrecipes.ui.helpers.MealSavingHelper
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
+@Suppress("OPT_IN_USAGE")
 class SearchViewModel(
     private val repository: SearchRepository = SearchRepository(),
     private val mealRepository: MealRepository = MealRepository(),
@@ -22,13 +27,23 @@ class SearchViewModel(
     private val _meals = MutableStateFlow<List<MealDetailsModel>>(emptyList())
     val meals = _meals.asStateFlow()
 
+    private val searchInput = MutableStateFlow("")
+
+    init {
+        searchInput
+            .debounce(SEARCH_DEBOUNCE)
+            .filter(String::isNotBlank)
+            .onEach(::searchForMeals)
+            .launchIn(viewModelScope)
+    }
+
     fun onSearchInputChange(text: String = "") {
         if (text.isBlank()) {
             _meals.value = emptyList()
             return
         }
 
-        searchForMeals(text)
+        searchInput.value = text
     }
 
     fun onSaveIconClick(meal: MealDetailsModel) {
@@ -43,11 +58,14 @@ class SearchViewModel(
 
     private fun searchForMeals(text: String) {
         viewModelScope.launch {
-            val response = repository.searchMeals(text)
-
-            if (response is ApiResponse.Success) {
-                _meals.value = response.data
-            }
+            repository
+                .searchMeals(text)
+                .onSuccess { _meals.value = this }
         }
+    }
+
+    companion object {
+
+        private const val SEARCH_DEBOUNCE = 400L
     }
 }
