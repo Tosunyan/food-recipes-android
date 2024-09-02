c85c4f6b0acf744da8cb2c05b76703bd5aee494f
Sergey Tosunyan <sergeytosunyan33@gmail.com>
2024-09-02 12:19:48 +0400

    Configure app signing

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Welcome to the **Food Recipes** Android application!
 This project, developed by [Sergey Tosunyan](https://www.linkedin.com/in/sergey-tosunyan-543882216/), serves as both as a personal portfolio and a reference for other developers. The goal is to showcase best practices in Android development using modern technologies and architectural patterns.
 
 ## Overview
-**Food Recipes** is an Android app that provides users with various recipes from around the world. It fetches data from ~[TheMealDB API](https://www.themealdb.com/api.php)~, allowing users to explore recipes by categories, regions, and more.
+**Food Recipes** is an Android app that provides users with various recipes from around the world. It fetches data from [TheMealDB API](https://www.themealdb.com/api.php), allowing users to explore recipes by categories, regions, and more.
 
 ## Features
 * **Daily Special**: Discover a new featured recipe every day
@@ -27,7 +27,12 @@ To set up this project on your local machine, follow these steps:
 1. Clone the repository
 2. Setup `local.properties` file in the root directory
     ```properties
+    KEY_ALIAS=food-recipes
+    KEY_PASSWORD=iM3hQqO1s1wUtDqr
+    STORE_PASSWORD=iM3hQqO1s1wUtDqr
+
     MEAL_API_KEY="1"
     MEAL_API_BASE_URL="https://www.themealdb.com/api/json/v1"
     ```
-3. Sync and run the project
+3. Create `prod.properties` file in the root directory, with [required properties](https://docs.google.com/spreadsheets/d/1MuSYiBYV134sgj6BprrIJxMGM5QlVHRFwMjyO-bLQik/edit?usp=drive_link):
+4. Sync and run the project
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,13 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import org.jetbrains.kotlin.konan.properties.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
+    var properties = gradleLocalProperties(rootDir, providers)
 
     compileSdk = 34
 
@@ -17,13 +21,42 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("$rootDir/debug.keystore")
+            storePassword = properties.getProperty("STORE_PASSWORD")
+            keyAlias = properties.getProperty("KEY_ALIAS")
+            keyPassword = properties.getProperty("STORE_PASSWORD")
+        }
+
+        create("release") {
+            properties = Properties().apply {
+                load(rootProject.file("prod.properties").inputStream())
+            }
+
+            storeFile = file("$rootDir/release.keystore")
+            storePassword = properties.getProperty("STORE_PASSWORD")
+            keyAlias = properties.getProperty("KEY_ALIAS")
+            keyPassword = properties.getProperty("STORE_PASSWORD")
+        }
+    }
+
     buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+
+            signingConfig = signingConfigs.getByName("debug")
+        }
+
         getByName("release") {
             isMinifyEnabled = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
