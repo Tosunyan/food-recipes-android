9f8d59fb623a7c8395608616ccbbaa168380a58f
Sergey Tosunyan <sergeytosunyan33@gmail.com>
2025-02-09 16:49:45 +0400

    Set compileOptions to 17

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -11,6 +11,11 @@ android {
         minSdk = 26
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
