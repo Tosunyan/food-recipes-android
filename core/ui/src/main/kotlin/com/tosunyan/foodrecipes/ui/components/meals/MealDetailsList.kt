f2bcfd8b1d6403923b6652a85ff5a3c8ca958372
Sergey Tosunyan <sergeytosunyan33@gmail.com>
2025-02-14 19:40:51 +0400

    Hide keyboard on list scroll

--- core/ui/src/main/kotlin/com/tosunyan/foodrecipes/ui/components/meals/MealDetailsList.kt
+++ core/ui/src/main/kotlin/com/tosunyan/foodrecipes/ui/components/meals/MealDetailsList.kt
@@ -6,8 +6,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import com.tosunyan.foodrecipes.model.MealDetailsModel
 
@@ -18,7 +21,17 @@ fun MealDetailsList(
     onItemClick: (MealDetailsModel) -> Unit = { },
     onSaveIconClick: (MealDetailsModel) -> Unit = { },
 ) {
+    val lazyListState = rememberLazyListState()
+    val focusManager = LocalFocusManager.current
+
+    LaunchedEffect(Unit) {
+        lazyListState.interactionSource.interactions.collect {
+            focusManager.clearFocus()
+        }
+    }
+
     LazyColumn(
+        state = lazyListState,
         contentPadding = contentPadding,
         verticalArrangement = Arrangement.spacedBy(24.dp),
         modifier = Modifier.fillMaxSize()
